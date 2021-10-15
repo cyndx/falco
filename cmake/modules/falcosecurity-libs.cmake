a62c615072bb25e8d663c223d938d9aad6e3b425
Mark Stemm <mark.stemm@gmail.com>
2021-10-15 16:23:42 -0700

    Update falcosecurity/libs version
    This moves up the commit to one that has plugins support.
    
    Co-authored-by: Leonardo Grasso <me@leonardograsso.com>
    Co-authored-by: Loris Degioanni <loris@sysdig.com>
    Signed-off-by: Mark Stemm <mark.stemm@gmail.com>

--- cmake/modules/falcosecurity-libs.cmake
+++ cmake/modules/falcosecurity-libs.cmake
@@ -20,8 +20,8 @@ file(MAKE_DIRECTORY ${FALCOSECURITY_LIBS_CMAKE_WORKING_DIR})
 # default below In case you want to test against another falcosecurity/libs version just pass the variable - ie., `cmake
 # -DFALCOSECURITY_LIBS_VERSION=dev ..`
 if(NOT FALCOSECURITY_LIBS_VERSION)
-  set(FALCOSECURITY_LIBS_VERSION "a03ccfda795f2ba711b80f69cb06869f2b63121b")
-  set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=97ce5c30b985b77e1abb04ef7037c69be176cbe2122acf67a7f6ec6b39dbdc27")
+  set(FALCOSECURITY_LIBS_VERSION "f7877aaf46fadae787fc852a042c58ae1fafd517")
+  set(FALCOSECURITY_LIBS_CHECKSUM "SHA256=26c08beae0f3ba31c07fbcd62f657ed6b486c648bc06669b07fba7fa5bb0bf25")
 endif()
 
 # cd /path/to/build && cmake /path/to/source
